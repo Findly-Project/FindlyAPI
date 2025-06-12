68108c017de675075001c55e4aaaba8b8e03e5de
kolo <kolo.is.main@gmail.com>
2025-06-12 21:47:49 +0300

    fullll refactor, redesign, decompose and etc

--- src/services/collecting_primary_data/get_mmg_data.py
+++ src/services/collecting_primary_data/get_mmg_data.py
@@ -2,7 +2,7 @@ import httpx
 from httpx import Response
 from bs4 import BeautifulSoup
 import logging
-from src.utils.get_config.get_pars_config import GetParsConfig
+from src.utils.get_config import GetParsConfig
 from .product_models import ProductData, ProductList
 import re
 
--- src/utils/get_config.py
+++ src/utils/get_config.py
@@ -4,28 +4,24 @@ import tomllib
 class GetParsConfig:
     @staticmethod
     def get_21vek_pars_config() -> dict[str]:
-        with open("src/secret_data/config.toml", "rb") as config:
+        with open("secret_data/config.toml", "rb") as config:
             pars_21vek_config: dict[str] = tomllib.load(config)["Pars"]["21vek"]
-
         return pars_21vek_config
 
     @staticmethod
     def get_kufar_pars_config() -> dict[str]:
-        with open("src/secret_data/config.toml", "rb") as config:
+        with open("secret_data/config.toml", "rb") as config:
             pars_kufar_config: dict[str] = tomllib.load(config)["Pars"]["Kufar"]
-
         return pars_kufar_config
 
     @staticmethod
     def get_mmg_pars_config() -> dict[str]:
-        with open("src/secret_data/config.toml", "rb") as config:
+        with open("secret_data/config.toml", "rb") as config:
             pars_mmg_config: dict[str] = tomllib.load(config)["Pars"]["MMG"]
-
         return pars_mmg_config
 
     @staticmethod
     def get_onliner_pars_config() -> dict[str]:
-        with open("src/secret_data/config.toml", "rb") as config:
+        with open("secret_data/config.toml", "rb") as config:
             pars_onliner_config: dict[str] = tomllib.load(config)["Pars"]["Onliner"]
-
         return pars_onliner_config
